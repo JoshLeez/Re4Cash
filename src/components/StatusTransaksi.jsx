7aedb84a230e11f13f232e56ee55d541b3320bfe
JoshLeez <leejosh500@gmail.com>
2023-01-11 23:10:45 +0700

    menambah pagination pada halaman akun profile

---
+++ src/components/StatusTransaksi.jsx
@@ -0,0 +1,33 @@
+import "./styles/paginationpesanansaya.css";
+
+const StatusTransaksi = () => {
+  return (
+    <div  className="content-pagination-pesanan-saya">
+      <div className="pesanan-progress">
+        <div className="left-pesanan-progress">
+          <img src="/serabut-kelapa.svg"/>
+          <div className="detail-pesanan-kami">
+            <h3>Serabut Kelapa</h3>
+            <p>Ambil Sendiri</p>
+            <h2>Bank Sampah Aisyah</h2>
+          </div>
+        </div>
+        <div className="right-pesanan-progress">
+          <p>x3</p>
+          <p>Rp10.000/kg</p>
+          <p>Total Transaksi : Rp45.000</p>
+          <div className="bank-menunggu-transaksi">
+            <img src="/bri.svg"/>
+            <h6>Menunggu Transaksi dalam 23.34.09 Jam</h6>
+          </div>
+          <div className="batalkan-transaksi">
+            <h6>Batalkan Transaksi</h6>
+            <iconify-icon icon="gridicons:cross-circle"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StatusTransaksi
--- src/pages/AkunProfile.jsx
+++ src/pages/AkunProfile.jsx
@@ -1,19 +1,59 @@
 import { HOCmarketplace } from "../components/HOC"
 import ProfileCard from "../components/ProfileCard"
+import StatusTransaksi from "../components/StatusTransaksi"
 import "./styles/akunprofile.css"
+import { NavLink } from "react-router-dom"
+import { useState} from "react"
 
 const AkunProfile = () => {
+
+  const [pagination, setPagination] = useState(0)
+  const [active, setActive] = useState(true)
+
+  const style = {
+      transform: `translateX(${pagination}px)`
+  };
+
+  const className1 = active ? "pesanan-saya-menu" : "pesanan-saya-menu active"
+
   return (
     <HOCmarketplace>
         <div className="container-akun-profile">
             <ProfileCard/>
-            <section className="wrapper-pesanan-saya">
+            <section className="akun-profile-right-content">
+              <div className="wrapper-pesanan-saya">
               <h1>Pesanan Saya</h1>
-              <div className="detail-pesanan-saya">
+              <section className="detail-pesanan-saya">
                 <header className="pesanan-saya-navigation">
-
+                  <div onClick={()=>{setPagination(0), setActive(false)}}  className={className1}>
+                    <iconify-icon icon="material-symbols:payments-outline-rounded"/>
+                    <h3>Status Transaksi</h3>
+                    <span className="circle-notification">1</span>
+                  </div>
+                  <div onClick={()=>setPagination(-904)} className={active ? "pesanan-saya-menu" : "pesanan-saya-menu active"}>
+                    <iconify-icon icon="lucide:package-open"/>
+                    <h3>Dikemas</h3>
+                    <span className="circle-notification">3</span>
+                  </div>
+                  <div  onClick={()=>setPagination(-1808)} className="pesanan-saya-menu">
+                    <iconify-icon icon="tabler:truck-delivery"/>
+                    <h3>Dikirim</h3>
+                  </div>
+                  <div  onClick={()=>setPagination(-2712)} className="pesanan-saya-menu">
+                  <iconify-icon icon="lucide:package-check"/>
+                    <h3>Pesanan Selesai</h3>
+                    <span className="circle-notification">1</span>
+                  </div>
                 </header>
+                <section className="content-nav-pesanan-saya">
+                    <StatusTransaksi/>
+                     <StatusTransaksi/>
+                </section>
+              </section>
               </div>
+              <section className="profile-setting-wrapper">
+
+              </section>
             </section>
         </div>
     </HOCmarketplace>
